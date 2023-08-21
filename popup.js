a9bedaae4ed07faaa0690051280f874b02f15311
Varma1959 <varma.pothuri999@gmail.com>
2023-08-21 15:53:15 +0530

    Added some more functionality

--- popup.js
+++ popup.js
@@ -19,60 +19,64 @@ document.addEventListener("DOMContentLoaded", function () {
 const api_url =
   "https://app.blinkcrm.in/api/whatsapp?api_key=19a5c89fb2ef43c1969d00e4554ab99e";
 
-// Defining async function
-async function getapi(url) {
-  // Storing response
-  const response = await fetch(url);
+// Add an event listener when the popup is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  getapi(api_url);
+});
 
-  // Storing data in form of JSON
-  var data = await response.json();
-  console.log(data);
-  if (response.ok) {
+async function getapi(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
     hideloader();
     show(data);
+  } catch (error) {
+    console.error("Error fetching API data:", error);
   }
 }
 
-// Calling that async function
-getapi(api_url);
-
-// Function to hide the loader
 function hideloader() {
   var loadingElement = document.getElementById("loading");
   loadingElement.style.display = "none";
 }
 
-// Function to define innerHTML for HTML table
 function show(data) {
   var tab = "<tr><th>Name</th><th>Phone</th></tr>";
 
-  // Loop to access all rows
   for (var i = 0; i < data.leads.length; i++) {
     var r = data.leads[i];
-
-   tab += "<tr><td>" + r.name + "</td><td><a href='javascript:void(0);' onclick=' onMobileNumberClick(\"" + r.phone + "\")'>" + r.phone + "</a></td></tr>";
+    tab +=
+      "<tr><td>" +
+      r.name +
+      "</td><td><a href='javascript:void(0);' data-phone='" +
+      r.phone +
+      "'>" +
+      r.phone +
+      "</a></td></tr>";
   }
 
-  //   tab += "<tr><td>" + r.name + "</td><td>" + r.phone + "</td></tr>";
-  // }
-
-  // Setting innerHTML as tab variable
   var employeesElement = document.getElementById("employees");
   employeesElement.innerHTML = tab;
+
+  // Attach event listeners to phone number links
+  const phoneLinks = document.querySelectorAll("a[data-phone]");
+  phoneLinks.forEach((link) => {
+    link.addEventListener("click", onMobileNumberClick);
+  });
 }
 
-// function handlePhoneClick(phoneNumber) {
-//   alert("Clicked on phone number: " + phoneNumber);
-//   // You can replace the alert with your desired action
-// }
-
- function onMobileNumberClick (phone, message) {
-   var message =
-     "Good Morning!\r\nHope you are doing well, We are connected with you over linkedin, I am *Shyam Mohan K, Founder \u0026 CEO at RazorOps, Inc.*  I like to Introduce RazorOps - The Simplest Container Native SaaS CI/CD platform! 🚀 \r\n\r\nWe recently helped an AI/ML, edTech, FinTech and Gaming companies to scale infra and CICD process to ship code changes fast. I want to see if there is any opportunity where you can use RazorOps DevOps Solution to automation CI/CD process.\r\n \r\n*Schedule a meeting*, In case if you like to know more or Start a *FREE POC https://bit.ly/3YEEtQy*  \r\n\r\nThanks \u0026 regards,\r\nShyam Mohan K (Founder \u0026 CEO)\r\n";
-   var url =
-     "https://api.whatsapp.com/send?phone=" +
-     phone +
-     "&text=" +
-     encodeURIComponent(message); // Change "message" to "Hi"
-   window.open(url, "_blank");
- };
+function onMobileNumberClick(event) {
+  event.preventDefault();
+  const phone = event.target.getAttribute("data-phone");
+  const message =
+    " Good day there, I'm Prasanna Varma, Frontend Developer from Hyderabad";
+  const url =
+    "https://api.whatsapp.com/send?phone=" +
+    phone +
+    "&text=" +
+    encodeURIComponent(message);
+  window.open(url, "_blank");
+}
